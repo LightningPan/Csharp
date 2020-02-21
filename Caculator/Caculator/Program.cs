a271bfb76639c951b7276411a8ff6e772badbdb9
Pan Tan <1032189322@qq.com>
2020-02-21 21:43:33 +0800

    创建项目

---
+++ Caculator/Caculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Caculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String num1s = Console.ReadLine();
+            double num1 = checkNum(num1s);
+            string func = Console.ReadLine();
+            String num2s = Console.ReadLine();
+            double num2 = checkNum(num2s);
+            getResult(num1,num2,func);
+        }
+
+        static double checkNum(string num) {
+            try
+            {
+                double i = Double.Parse(num);
+                return i;
+            }
+            catch(Exception e) {
+                Console.WriteLine("输入有误");
+                string str = Console.ReadLine();
+                return checkNum(str);
+            }
+        
+        }
+        static void getResult(double num1,double num2,string func) {
+
+            double result = 0;
+            switch (func) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("输入有误");
+                    return;
+            }
+            Console.WriteLine(result);
+        
+        }
+    }
+}
---
+++ caculatorWin/caculatorWin/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace caculatorWin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text=$"{getResult(1)}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = $"{getResult(2)}";
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = $"{getResult(3)}";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = $"{getResult(4)}";
+        }
+
+        private double getResult(int n) {
+            try
+            {
+                double num1 = checkNum(textBox1.Text);
+                double num2 = checkNum(textBox2.Text);
+                double result;
+                switch (n)
+                {
+                    case 1:
+                        result= num1 + num2;
+                        break;
+                    case 2:
+                        result = num1 - num2;
+                        break;
+                    case 3:
+                        result = num1 * num2;
+                        break;
+                    case 4:
+                        result = num1 / num2;
+                        break;
+                    default:
+                        result = 0;
+                        break;
+                }
+                return result;
+            }
+            catch (Exception e) {
+                
+            }
+        
+        }
+        double checkNum(string num)
+        {
+                double i = Double.Parse(num);
+                return i;
+        }
+
+
+
+
+
+    }
+}
