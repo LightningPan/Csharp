e842dd642cd23172b1b6019440f43d64595b4303
LightningPan <tp1032189322@live.com>
2020-03-18 23:09:44 +0800

    第四次

---
+++ homework4/GenericApplication/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication {
+
+  // 链表节点
+  public class Node<T> {
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+
+    public Node(T t) {
+      Next = null;
+      Data = t;
+    }
+  }
+
+  //泛型链表类
+  public class GenericList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+
+        public void ForEach(Action<T> action) {
+            for (Node<T> x = head; x != null; x = x.Next) {
+
+                action(x.Data);
+            }
+        
+        }
+
+    
+
+    public GenericList() {
+      tail = head = null;
+    }
+
+    public Node<T> Head {
+      get => head;
+    }
+
+    public void Add(T t) {
+      Node<T> n = new Node<T>(t);
+      if (tail == null) {
+        head = tail = n;
+      }else {
+        tail.Next = n;
+        tail = n;
+      }
+    }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      // 整型List
+          GenericList<int> intlist = new GenericList<int>();
+          for (int x = 0; x < 10; x++) {
+            intlist.Add(x);
+          }
+          intlist.ForEach(x=>Console.WriteLine(x));
+            int sum = 0, max = 0, min = 0;
+            intlist.ForEach(x => sum += x);
+            intlist.ForEach(x => { if (max < x) max = x; });
+            intlist.ForEach(x=> { if (min > x) min = x; });
+            Console.WriteLine(sum);
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+            //for (Node<int> node = intlist.Head; 
+            //      node != null; node = node.Next) {
+            //  Console.WriteLine(node.Data);
+            //}
+
+            //// 字符串型List
+            //GenericList<string> strList = new GenericList<string>();
+            //for (int x = 0; x < 10; x++) {
+            //  strList.Add("str"+x);
+            //}
+            //for (Node<string> node = strList.Head; 
+            //        node != null; node = node.Next) {
+            //  Console.WriteLine(node.Data);
+            //}
+
+
+        }
+
+  }
+}
---
+++ homework4/clock/clock/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace clock
+{
+    public delegate void ClockAction(int args);
+    public class ClockEvent {
+        public event ClockAction OnTick;
+        public event ClockAction OnAlarm;
+
+        public void Tick(int tick) {  
+           
+            OnTick(tick);
+        }
+        public void Alarm(int alarm) {
+            
+            OnAlarm(alarm);
+        }
+    }
+    public class Clock {
+        public ClockEvent clock1 = new ClockEvent();
+        private int tick = 0;
+        private int alarm = 0;
+
+        public void setAlarm(int var) {
+            alarm = var;
+        }
+
+        public Clock() {
+            alarm = 0;
+            clock1.OnAlarm += new ClockAction(Clock_OnAlarm);
+            clock1.OnTick += new ClockAction(Clock_OnTick);
+        }
+        void Clock_OnTick(int tick) {
+            Console.WriteLine("嘀嗒"+" 现在是"+tick);
+        }
+        void Clock_OnAlarm(int alarm) {
+            Console.WriteLine(alarm+"到了"+"嗡嗡嗡");
+        }
+        public void ClockRun() {
+            while (true) {
+                clock1.Tick(tick) ;
+                if (tick == alarm) {
+                    clock1.Alarm(alarm);
+                }
+                tick++;
+                System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock test = new Clock();
+            test.setAlarm(10);
+            test.ClockRun();
+            
+        }
+    }
+
+}
