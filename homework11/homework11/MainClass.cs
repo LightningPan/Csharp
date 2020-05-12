fc52ce2908fa11c7bfc98ec4ba3e17bda3f4ab9f
LightningPan <tp1032189322@live.com>
2020-05-12 23:49:01 +0800

    11

---
+++ homework11/homework11/MainClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework11
+{
+    class MainClass
+    {
+        static void Main(string[] args) {
+            OrderService a = new OrderService();
+            a.AddOrder("hello");
+
+        }
+    }
+}
---
+++ homework11/homework11/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework11
+{
+    class Order
+    {
+        public int OrderId { get; set; }
+        public String Customer { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}
