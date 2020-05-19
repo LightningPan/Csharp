c8f5db79deda18cb02789c73aa14d833292d04de
LightningPan <tp1032189322@live.com>
2020-05-20 02:06:38 +0800

    12

---
+++ homework12/homework12/Controllers/OrderController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OrderApp;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace homework12.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+        private readonly OrderContext OrderDatabase;
+        public OrderController(OrderContext context) {
+            this.OrderDatabase = context;
+        }
+        // GET: api/<controller>
+      
+
+        [HttpGet]
+        public ActionResult<List<Order>> GetOrder(string goodsName,string customerName,float? totalAmount) {
+            IQueryable<Order> query = OrderDatabase.Orders;
+            if (goodsName != null) { 
+                query=query.Where(o => o.Items.Count(i => i.GoodsItem.Name == goodsName) > 0);
+            }
+            if (customerName != null) { 
+                query=query.Where(o => o.Customer.Name == customerName); 
+            }
+            if (totalAmount != null) {
+                query = query.Where(o => o.Items.Sum(item => item.GoodsItem.Price * item.Quantity) > totalAmount);
+            }
+            return query.ToList();
+        
+        }
+
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public ActionResult<Order> Get(string id)
+        {
+            var order = OrderDatabase.Orders.FirstOrDefault(o => o.Id == id);
+            if (order==null) {
+                return NotFound();
+            }
+            return order;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public ActionResult<Order> Post(Order order)
+        {
+            try
+            {
+                OrderDatabase.Orders.Add(order);
+                OrderDatabase.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+            return order;
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public ActionResult<Order> Put(string id,Order order)
+        {
+            if (id != order.Id) { 
+                return BadRequest("Id cannot be modified");
+            }
+            try {
+                OrderDatabase.Entry(order).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                OrderDatabase.SaveChanges();
+            }
+            catch (Exception e) {
+                string error = e.Message;
+                if (e.InnerException != null) error = e.InnerException.Message;
+                return BadRequest(error);
+            }
+            return NoContent();
+
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            try {
+                var order = OrderDatabase.Orders.FirstOrDefault(t => t.Id == id);
+                if (order != null) {
+                    OrderDatabase.Remove(order);
+                    OrderDatabase.SaveChanges();
+                }
+            }
+            catch (Exception e) {
+                return BadRequest(e.InnerException.Message);
+            }
+            return NoContent();
+        }
+    }
+}
---
+++ homework12/homework12/OrderApp/OrderContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp {
+  public class OrderContext : DbContext {
+
+    public OrderContext(DbContextOptions<OrderContext> options) : base(options) {
+            this.Database.EnsureCreated();
+    }
+
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Goods> GoodItems { get; set; }
+    public DbSet<Customer> Customers{ get; set; }
+
+  }
+}
